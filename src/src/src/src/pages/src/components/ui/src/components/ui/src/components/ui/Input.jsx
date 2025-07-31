7327948461f3270157149a478725e61b621ac90d
Walefrncs <walefrncs@gmail.com>
2025-07-31 12:46:41 +0100

    Create Input.jsx

---
+++ src/src/src/src/pages/src/components/ui/src/components/ui/src/components/ui/Input.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Input({ className, ...props }) {
+  return (
+    <input
+      className={`w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${className || ""}`}
+      {...props}
+    />
+  );
