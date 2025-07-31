f1022b8c5f88c295b75e19c50c8d032a08c51915
Walefrncs <walefrncs@gmail.com>
2025-07-31 12:36:57 +0100

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import KidsGadgetsHome from './pages/KidsGadgetsHome'
+
+export default function App() {
+  return (
+    <main>
+      <KidsGadgetsHome />
+    </main>
+  )
